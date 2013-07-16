bc98faaad626077c1e4e1d72466bede2a930c8a5
Roman Palkin <roman@palkin.info>
2013-07-16 16:28:15 +0700

    [*] quote to quote_plus changed

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def md5(s):
     return hashlib.md5(s).hexdigest()
     
 def http_build_query(params, topkey = ''):
-    from urllib import quote
+    from urllib import quote_plus
 
     if len(params) == 0:
         return ""
@@ -31,9 +31,9 @@ def http_build_query(params, topkey = ''):
         for key in params.keys():
             value = params[key]
 
-            newkey = quote (key)
+            newkey = quote_plus (key)
             if topkey != '':
-                newkey = topkey + quote('[' + key + ']')
+                newkey = topkey + quote_plus('[' + key + ']')
 
             if type(value) is dict:
                 result += http_build_query (value, newkey)
@@ -41,21 +41,26 @@ def http_build_query(params, topkey = ''):
             elif type(value) is list:
                 i = 0
                 for val in value:
-                    result += newkey + quote('[' + str(i) + ']') + "=" + quote(str(val)) + "&"
+#                    result += newkey + quote_plus('[' + str(i) + ']') + "=" + quote_plus(str(val)) + "&"
+                    result += http_build_query(val, newkey + quote_plus('[' + str(i) + ']'))
                     i = i + 1
 
             # boolean should have special treatment as well
             elif type(value) is bool:
-                result += newkey + "=" + quote (str(int(value))) + "&"
+                result += newkey + "=" + quote_plus (str(int(value))) + "&"
+
+            elif type(value) is unicode:
+                value = value.encode('UTF-8')
+                result += newkey + "=" + quote_plus (str(value)) + "&"
 
             # assume string (integers and floats work well)
             else:
-                result += newkey + "=" + quote (str(value)) + "&"
+                result += newkey + "=" + quote_plus (str(value)) + "&"
     elif type(params) is list:
         for key, value in params:
-            newkey = quote (key)
+            newkey = quote_plus (key)
             if topkey != '':
-                newkey = topkey + quote('[' + key + ']')
+                newkey = topkey + quote_plus('[' + key + ']')
 
             if type(value) is dict:
                 result += http_build_query (value, newkey)
@@ -63,16 +68,23 @@ def http_build_query(params, topkey = ''):
             elif type(value) is list:
                 i = 0
                 for val in value:
-                    result += newkey + quote('[' + str(i) + ']') + "=" + quote(str(val)) + "&"
+#                    result += newkey + quote_plus('[' + str(i) + ']') + "=" + quote_plus(str(val)) + "&"
+                    result += http_build_query(val, newkey + quote_plus('[' + str(i) + ']'))
                     i = i + 1
 
             # boolean should have special treatment as well
             elif type(value) is bool:
-                result += newkey + "=" + quote (str(int(value))) + "&"
+                result += newkey + "=" + quote_plus (str(int(value))) + "&"
+
+            elif type(value) is unicode:
+                value = value.encode('UTF-8')
+                result += newkey + "=" + quote_plus (str(value)) + "&"
 
             # assume string (integers and floats work well)
             else:
-                result += newkey + "=" + quote (str(value)) + "&"
+                result += newkey + "=" + quote_plus (str(value)) + "&"
+    else:
+        result += topkey + "=" + quote_plus(str(params)) + "&"
 
     # remove the last '&'
     if (result) and (topkey == '') and (result[-1] == '&'):
